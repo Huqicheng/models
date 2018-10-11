a4ffd03bb7b9b73b854e94b6fb8c056a3a7f6c36
Qicheng Hu <31775227+Huqicheng@users.noreply.github.com>
2018-10-11 00:54:07 -0400

    Update train.py

--- research/object_detection/legacy/train.py
+++ research/object_detection/legacy/train.py
@@ -56,7 +56,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 ####Delete all flags before declare#####
 def del_all_flags(FLAGS):
-    flags_dict = FLAGS.flags()    
+    flags_dict = FLAGS._flags()    
     keys_list = [keys for keys in flags_dict]    
     for keys in keys_list:    
         FLAGS.__delattr__(keys)
